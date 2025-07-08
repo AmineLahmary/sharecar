1499f61b608fa0317c1974fb88943b3d56e054ca
Amine Lahmary <aminelahmary@gmail.com>
2025-07-08 21:01:25 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="app-container">
+    <img src="https://images.unsplash.com/photo-1565043666747-69f6646db940?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+    <h1>ShareCar</h1>
+    <p>Comming soon...</p>
+  </div>
+</template>
+
+
